d70f36d56d830e34ce4cd2c131998847ef18dfba
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2021-06-14 01:45:42 +0300

    improved two lists algorathims

--- SingleLinkedOrderedList/SingleLinkedOrderedList.h
+++ SingleLinkedOrderedList/SingleLinkedOrderedList.h
@@ -1,41 +1,62 @@
-//#ifndef __Queue
-//#define __Queue
+#ifndef Single_Linked_List
+#define Single_Linked_List
+
+//  SingleLinkedList.h - Single Linked List of integers
 //
-//class Node
-//{
-//public:
-//	int data;
-//	Node* next;
-//
-//	Node(int d = 0, Node* next = nullptr);
-//};
-//
-//class SingleLinkedList
-//{
-//public:
-//	Node* head;
-//
-//SingleLinkedList(Node* n = nullptr);
-//
-//Node* FindeItem(int d);
-//
-//void sortList();
-//
-//void InsertItem(int n);
-//
-//void DeleteItem(int n);
-//
-//void UpdateNode(int k, int d);
-//
-//void ShowList();
-//};
-//
-//void MoveItem(int n, SingleLinkedList from, SingleLinkedList to);
-//
-//void DeleteCommon(SingleLinkedList& first, SingleLinkedList& second);
-//
-//SingleLinkedList CommonItems(SingleLinkedList* first, SingleLinkedList* second);
-//
-//SingleLinkedList MergeLists(SingleLinkedList* first, SingleLinkedList* second);
-//
-//#endif
+
+class SingleLinkedList
+{
+public:
+	struct Node
+	{
+		int data;
+		Node *next; // Next node in the list
+
+		// Constructor for creating a new list item.
+		Node(int d = 0, Node *next = nullptr)
+		{
+			this->data = d;		 // value of the list item
+			this->next = next; // pointer to the next item in the list
+		}
+	};
+
+	Node *head; // first element of the list
+
+	// "default" constructor - create an empty list
+	SingleLinkedList(Node *n = nullptr);
+
+	// Find item in the list
+	Node *FindeItem(int d);
+
+	// Sort list in order
+	void sortList();
+
+	// Add item to the list
+	void InsertItem(int n);
+
+	// Delete item from the list
+	void DeleteItem(int n);
+
+	// Update item in the list
+	void UpdateNode(int k, int d);
+
+	// Display all items in the list
+	void ShowList();
+
+	// List destructor
+	~SingleLinkedList();
+};
+
+// Move item from list to list
+void MoveItem(int n, SingleLinkedList from, SingleLinkedList to);
+
+// Delete common items in the list
+void DeleteCommon(SingleLinkedList &first, SingleLinkedList &second);
+
+// Display the common items in two lists
+SingleLinkedList CommonItems(SingleLinkedList *first, SingleLinkedList *second);
+
+// Merge two lists
+SingleLinkedList MergeLists(SingleLinkedList *first, SingleLinkedList *second);
+
+#endif
--- SingleLinkedOrderedList/main.cpp
+++ SingleLinkedOrderedList/main.cpp
@@ -1,62 +1,63 @@
-//#include <iostream>
-//#include "SingleLinkedOrderedList.h"
-//using namespace std;
-//
-//int main()
-//{
-//	SingleLinkedList A;
-//	cout << "list A created " << endl;
-//	A.InsertItem(1);
-//	A.InsertItem(4);
-//	A.InsertItem(3);
-//	A.InsertItem(2);
-//	A.InsertItem(12);
-//	A.InsertItem(5);
-//	A.InsertItem(8);
-//	cout << "list A : ";
-//	A.ShowList();
-//	A.DeleteItem(2);
-//	A.ShowList();
-//	A.UpdateNode(1, 6);
-//	A.ShowList();
-//
-//	SingleLinkedList B;
-//	cout << "list B created " << endl;
-//	B.InsertItem(3);
-//	B.InsertItem(12);
-//	B.InsertItem(2);
-//	B.InsertItem(11);
-//	B.InsertItem(5);
-//	B.InsertItem(16);
-//	B.InsertItem(14);
-//	B.InsertItem(22);
-//	B.InsertItem(9);
-//	B.InsertItem(13);
-//	cout << "list B : ";
-//	B.ShowList();
-//	cout << "=====================" << endl;
-//	cout << endl;
-//	
-//	SingleLinkedList C;
-//	cout << "list C created " << endl;
-//	cout << "Common items between A and B :" << endl;;
-//	C = CommonItems(&A, &B);
-//	C.ShowList();
-//	cout << endl;
-//	
-//	cout << "move item from A to B :" << endl;
-//	MoveItem(8, A, B);
-//	cout << endl;
-//	
-//	SingleLinkedList D;
-//	cout << "merged list" << endl;
-//	D = MergeLists(&A, &B);
-//	D.ShowList();
-//	cout << endl;
-//
-//	cout << "delete commnon" << endl;
-//	DeleteCommon(A, B);
-//	B.ShowList();
-//
-//	return 0;
-//}
+#include <iostream>
+#include "SingleLinkedOrderedList.h"
+using namespace std;
+
+int main()
+{
+	SingleLinkedList A;
+	cout << "list A created " << endl;
+	A.InsertItem(1);
+	A.InsertItem(4);
+	A.InsertItem(3);
+	A.InsertItem(2);
+	A.InsertItem(12);
+	A.InsertItem(5);
+	A.InsertItem(8);
+	cout << "list A : ";
+	A.ShowList();
+	A.DeleteItem(2);
+	A.ShowList();
+	A.UpdateNode(1, 6);
+	A.ShowList();
+
+	SingleLinkedList B;
+	cout << "list B created " << endl;
+	B.InsertItem(3);
+	B.InsertItem(12);
+	B.InsertItem(2);
+	B.InsertItem(11);
+	B.InsertItem(5);
+	B.InsertItem(16);
+	B.InsertItem(14);
+	B.InsertItem(22);
+	B.InsertItem(9);
+	B.InsertItem(13);
+	cout << "list B : ";
+	B.ShowList();
+	cout << "=====================" << endl;
+	cout << endl;
+
+	SingleLinkedList C;
+	cout << "list C created " << endl;
+	cout << "Common items between A and B :" << endl;
+	;
+	C = CommonItems(&A, &B);
+	C.ShowList();
+	cout << endl;
+
+	cout << "move item from A to B :" << endl;
+	MoveItem(8, A, B);
+	cout << endl;
+
+	SingleLinkedList D;
+	cout << "merged list" << endl;
+	D = MergeLists(&A, &B);
+	D.ShowList();
+	cout << endl;
+
+	cout << "delete commnon" << endl;
+	DeleteCommon(A, B);
+	B.ShowList();
+
+	return 0;
+}
